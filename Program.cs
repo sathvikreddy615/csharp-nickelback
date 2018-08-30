04881e570c44773ab5712ba00dbc14a88d34b0d0
NSS4 <nss4@NSS4s-MacBook-Pro.local>
2018-08-30 15:21:56 -0500

    completed exercise

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string artist, string song)> goodSongs = new List<(string artist, string song)>();
+            HashSet<(string artist, string song)> allSongs = new HashSet<(string artist, string song)>() {
+                {("Red Hot Chili Peppers", "Dani California")},
+                {("Nirvana", "Come As You Are")},
+                {("Metallica", "Enter Sandman")},
+                {("Nickleback", "Burn It To The Ground")},
+                {("Nickleback", "Feelin' Way Too Damn Good")},
+                {("Nickleback", "Rockstar")},
+                {("Alt-J", "Every Other Freckle")}
+            };
+
+            foreach ((string artist, string song) song in allSongs)
+            {
+                if (song.artist != "Nickleback") {
+                    goodSongs.Add((song.artist, song.song));
+                }
+            }
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"A good artist such as {song.artist} sing good songs like {song.song}");
+            }
+        }
+    }
+}
